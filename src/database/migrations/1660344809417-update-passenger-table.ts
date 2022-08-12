a5f6516cf43f50d40fe549ac30f88c2ceea9fa5e
Onuchukwu Adebayo <adebayop.o@gmail.com>
2022-08-13 00:13:37 +0100

    feat: setup migration

--- src/database/migrations/1660344809417-update-passenger-table.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class updatePassengerTable1660344809417 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
